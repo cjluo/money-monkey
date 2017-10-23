be2aa489db3c0d781c68e81d750086b040bc2d57
Chenjie Luo <cjluo.zju@gmail.com>
2017-10-22 20:07:12 -0700

    Add stockdao to fetch daily data.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import json
 import logging
 
-from stock_data_saver import StockDataSaver
+from stock_dao import StockDao
 from alpha_vantage_api_service import AlphaVantageApiService
 
 
@@ -11,12 +11,14 @@ def main():
     parser.add_argument("-c", "--config", help="config file path")
     parser.add_argument("-s", "--symbol", help="symbols separated by ,")
     parser.add_argument("-m", "--model", help="model, specify daily or latest")
+    parser.add_argument("-n", "--n", help="data size of the ml model")
     args = parser.parse_args()
 
     symbols = args.symbol.split(',')
     if len(symbols) == 0:
         logging.error("No symbols specified")
         return
+    n = int(args.n)
 
     with open(args.config) as config_file:
         config = json.load(config_file)
@@ -26,23 +28,37 @@ def main():
                      config['DB_PATH'], config['ALPHA_VANTAGE_KEY'])
         logging.info("Symbol %s", str(symbols))
 
-        saver = StockDataSaver(config['DB_PATH'])
+        dao = StockDao(config['DB_PATH'])
         api_service = AlphaVantageApiService(config['ALPHA_VANTAGE_KEY'])
 
         for symbol in symbols:
             if args.model == 'latest':
                 models = api_service.get_latest(symbol)
+                # Reversed order
+                prev_models = dao.load_data(
+                    symbol, models[-1].timestamp.date(), n - 1)
+                close = []
+                for prev_model in prev_models:
+                    close.append(prev_model.close)
+                close.append(models[-1].close)
+
             elif args.model == 'daily':
                 models = api_service.get_daily(symbol)
+                prev_models = models[-n:]
+                close = []
+                for prev_model in prev_models:
+                    close.append(prev_model.close)
+
             else:
                 logging.error("No model specified")
             logging.info("%s: %d entries added", symbol, len(models))
+            print close
             if len(models) == 0:
                 logging.error("No models added")
                 return
             logging.info("first entry: %s", models[0])
             logging.info("last entry: %s", models[-1])
-            saver.save_data(models)
+            dao.save_data(models)
 
 
 if __name__ == "__main__":
--- stock_dao.py
+++ stock_dao.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from stock_model_daily import StockModelDaily
+
 Base = declarative_base()
 
-class StockDataSaver:
+
+class StockDao:
     def __init__(self, db_path):
         self._engine = create_engine('sqlite:///' + db_path)
         Base.metadata.create_all(self._engine)
@@ -13,6 +16,15 @@ class StockDataSaver:
     def save_data(self, models):
         session = self._session()
         for model in models:
-            if not session.query(model.__class__).get((model.symbol, model.timestamp)):
+            if not session.query(model.__class__).get(
+                    (model.symbol, model.timestamp)):
                 session.add(model)
         session.commit()
+
+    def load_data(self, symbol, timestamp, n):
+        session = self._session()
+        data = session.query(StockModelDaily).filter(
+            StockModelDaily.symbol == symbol).filter(
+            StockModelDaily.timestamp < timestamp).order_by(
+            StockModelDaily.timestamp.desc()).limit(n).all()
+        return reversed(data)
